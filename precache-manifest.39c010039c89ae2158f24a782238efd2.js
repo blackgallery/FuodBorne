63673dda0ac605bcd304c4697d28b143951efc5a
Joni <pacificjoni@gmail.com>
2020-08-31 00:31:38 +0600

    Updates

---
+++ precache-manifest.39c010039c89ae2158f24a782238efd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a530d4419e1529f4ce6b9cd2c4fa7e",
+    "url": "/FuodBorne/index.html"
+  },
+  {
+    "revision": "edffa9d88378cabdd517",
+    "url": "/FuodBorne/static/css/2.0fd6ed1c.chunk.css"
+  },
+  {
+    "revision": "911e7abcca3a9a562a08",
+    "url": "/FuodBorne/static/css/main.e2034bd7.chunk.css"
+  },
+  {
+    "revision": "edffa9d88378cabdd517",
+    "url": "/FuodBorne/static/js/2.73794779.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/FuodBorne/static/js/2.73794779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911e7abcca3a9a562a08",
+    "url": "/FuodBorne/static/js/main.930abc13.chunk.js"
+  },
+  {
+    "revision": "d1376a69b4de32d96d65",
+    "url": "/FuodBorne/static/js/runtime-main.d4626fdf.js"
+  }
+]);
